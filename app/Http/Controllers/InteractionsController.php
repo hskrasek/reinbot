ad11fe641fbf0eb58cf6a4bf51984ba85657d4b9
Hunter Skrasek <hunterskrasek@me.com>
2017-07-03 16:07:50 -0500

    Update InteractionsController to handle RSVPs to a plan
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Http/Controllers/InteractionsController.php
+++ app/Http/Controllers/InteractionsController.php
@@ -2,20 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\PlanRepository;
+use App\UserRepository;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Http\Request;
 
 class InteractionsController extends Controller
 {
+    /**
+     * @var \App\UserRepository
+     */
+    private $users;
+
+    /**
+     * @var \App\PlanRepository
+     */
+    private $plans;
+
+    /**
+     * @var \Illuminate\Contracts\Translation\Translator
+     */
+    private $trans;
+
+    public function __construct(UserRepository $users, PlanRepository $plans, Translator $trans)
+    {
+        $this->users = $users;
+        $this->plans = $plans;
+        $this->trans = $trans;
+    }
+
     public function __invoke(Request $request)
     {
         $payload = json_decode($request->input('payload'), true);
 
-        $rsvp = array_get($payload, 'actions.0.value');
+        $user = $this->users->getBySlackId(array_get($payload, 'user.id'));
+        $plan = $this->plans->getById(explode('-', array_get($payload, 'callback_id'))[1]);
+
+        $response = (bool) array_get($payload, 'actions.0.value');
+
+        /** @var \App\Rsvp $rsvp */
+        if (!$rsvp = $plan->rsvps()->where('user_id', $user->id)->first()) {
+            $rsvp = $plan->rsvps()->create(['user_id' => $user->id]);
+        }
+
+        $rsvp->where(['user_id' => $user->id, 'plan_id' => $plan->id])->update(['response' => $response]);
 
         return [
-            'response_type'    => 'ephemeral',
-            "replace_original" => false,
-            'text'             => $rsvp === 'going' ? 'See you there!' : 'Sorry you can\'t make it',
+            'response_type'    => 'in_channel',
+            'replace_original' => false,
+            'text'             => $this->trans->trans('messages.plan_rsvp', [
+                'user_id' => $user->slack_user_id,
+                'mention' => $user->username,
+                'rsvp'    => $rsvp->response ? 'Going' : 'Can\'t go'
+            ]),
         ];
     }
 }
