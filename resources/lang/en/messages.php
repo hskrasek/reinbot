0bf515ce01f9c1cb734da8e8b3e37a25ef24985d
Hunter Skrasek <hunterskrasek@me.com>
2017-07-03 16:05:46 -0500

    Add a new lang key for RSVP text
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,4 +2,5 @@
 
 return [
     'plan_text' => '<!everyone>, <@:user_id|:mention> started a plan <!date^:time^{date_pretty} at {time}|:time_formatted>',
+    'plan_rsvp' => '<@:user_id|:mention> responded :rsvp',
 ];
