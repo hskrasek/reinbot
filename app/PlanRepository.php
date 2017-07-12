4ce15cb98755a51cfcb47fb72bd5dfe30487fcab
Hunter Skrasek <hunterskrasek@me.com>
2017-07-11 23:18:10 -0500

    Update PlanRepository to handle new command structure
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/PlanRepository.php
+++ app/PlanRepository.php
@@ -16,7 +16,7 @@ class PlanRepository
      *
      * @return \App\Plan
      */
-    public function getById($id)
+    public function getById(int $id) : Plan
     {
         return Plan::with('rsvps')->find($id);
     }
@@ -25,15 +25,16 @@ class PlanRepository
      * Creates a plan belonging to a user, based on command data.
      *
      * @param \App\User $user
-     * @param array     $data
+     * @param string    $time
+     * @param string    $responseUrl
      *
      * @return \App\Plan
      */
-    public function createPlan(User $user, $data)
+    public function createPlan(User $user, string $time, string $responseUrl): Plan
     {
         return tap($user->plans()->create([
-            'scheduled_at' => $this->getPlansScheduledTime($user, $data['text']),
-            'response_url' => $data['response_url'],
+            'scheduled_at' => $this->getPlansScheduledTime($user, $time),
+            'response_url' => $responseUrl,
         ]), function (Plan $plan) {
             $scheduledAt = clone $plan->scheduled_at;
             dispatch(
@@ -55,9 +56,9 @@ class PlanRepository
      *
      * @return \Carbon\Carbon
      */
-    protected function getPlansScheduledTime(User $user, $text)
+    protected function getPlansScheduledTime(User $user, string $text) : Carbon
     {
-        preg_match('/at (\d+(?>:\d+)?(?>am|pm|AM|PM)?)/i', $text, $matches);
+        preg_match('/(\d+(?>:\d+)?(?>am|pm|AM|PM)?)/i', $text, $matches);
 
         // Handle empty text here, which also covers incorrect input data
         if (empty($matches)) {
