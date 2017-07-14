11a25746536981687ae096ddd34a1e9d22b53a50
Hunter Skrasek <hunterskrasek@me.com>
2017-07-13 22:04:39 -0500

    Update the eloquent models with doc blocks
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Rsvp.php
+++ app/Rsvp.php
@@ -4,6 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Rsvp
+ *
+ * @property int $user_id
+ * @property int $plan_id
+ * @property bool $response
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rsvp whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rsvp wherePlanId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rsvp whereResponse($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rsvp whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Rsvp whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Rsvp extends Model
 {
     protected $fillable = [
--- app/User.php
+++ app/User.php
@@ -5,6 +5,26 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $slack_user_id
+ * @property string $username
+ * @property string|null $timezone
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read mixed $mention
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Plan[] $plans
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereSlackUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereTimezone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
