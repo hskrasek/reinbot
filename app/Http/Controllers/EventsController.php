85f4d4490c36589f017fb5f6f0e9dc89da57d4f4
Hunter Skrasek <hunterskrasek@me.com>
2017-08-06 20:40:08 -0500

    Update user id to reinbots
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,16 +9,17 @@ class EventsController extends Controller
 {
     public function __invoke(Request $request)
     {
+        \Log::debug('event', $request->input());
         /** @var BotMan $botMan */
         $botMan = app('botman');
 
-        $botMan->hears('<@U6A8LC5L6> start a plan for {time}', 'App\Commands\Bot\Handlers\CreatePlan@handle');
+        $botMan->hears('<@U6JUKRTE0> start a plan for {time}', 'App\Commands\Bot\Handlers\CreatePlan@handle');
         $botMan->hears(
-            '<@U6A8LC5L6> (?>who\'s|whos|who) (?>the|is a) bitch\?*',
+            '<@U6JUKRTE0> (?>who\'s|whos|who) (?>the|is a) bitch\?*',
             'App\Commands\Bot\Handlers\Bitch@handle'
         );
-        $botMan->hears('<@U6A8LC5L6> get overwatch rank for {gamertag}', 'App\Commands\Bot\Handlers\OWRank@handle');
-        $botMan->hears('<@U6A8LC5L6> help', function (BotMan $botMan) {
+        $botMan->hears('<@U6JUKRTE0> get overwatch rank for {gamertag}', 'App\Commands\Bot\Handlers\OWRank@handle');
+        $botMan->hears('<@U6JUKRTE0> help', function (BotMan $botMan) {
             $botMan->typesAndWaits(2)->reply('I currently am listening to:');
             $botMan->reply('@reinbot start a plan for 9pm');
             $botMan->reply('@reinbot get overwatch rank for Rev3rb');
