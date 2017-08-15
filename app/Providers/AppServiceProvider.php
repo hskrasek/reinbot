401eb7f97169c5772e96a502a6624c1cb1891c9c
Hunter Skrasek <hunterskrasek@me.com>
2017-08-14 21:57:32 -0500

    Add binding for Guzzle client that can handle Slack's dumb error responses
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Horizon\Horizon;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\choose_handler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,6 +41,33 @@ class AppServiceProvider extends ServiceProvider
 
         Horizon::auth(function ($request) {
             return $request->key = config('services.horizon.key');
+        });
+
+        $this->app->singleton(Client::class, function () {
+            $stack = new HandlerStack(choose_handler());
+            $stack->push(function (callable $handler) {
+                return function (
+                    RequestInterface $request,
+                    array $options
+                ) use ($handler) {
+                    $promise = $handler($request, $options);
+
+                    return $promise->then(
+                        function (ResponseInterface $response) use ($request) {
+                            $data = \GuzzleHttp\json_decode((string) $response->getBody(), true);
+
+                            if (!$data['ok']) {
+                                throw new RequestException($data['error'], $request, $response);
+                            }
+
+                            return $response;
+                        }
+                    );
+                };
+            });
+            $client = new Client(['handler' => $stack]);
+
+            return $client;
         });
     }
 }
