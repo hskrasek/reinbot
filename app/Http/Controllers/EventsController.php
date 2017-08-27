067373e3d7358fae451030ecd28b7041bf4dcd5f
Hunter Skrasek <hunterskrasek@me.com>
2017-08-27 11:24:24 -0500

    Remove testing hears
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -26,13 +26,13 @@ class EventsController extends Controller
             $botMan->reply('@reinbot get overwatch rank for Rev3rb');
         });
         $botMan->hears('<@U6JUKRTE0> debug', 'App\Commands\Bot\Handlers\Debug@handle');
-        $botMan->hears('create_plan', function (BotMan $botMan) {
-            $entities = $botMan->getMessage()->getExtras('entities');
-            $botMan->reply('Received entities: ' . json_encode($entities));
-//            $planTime = Carbon::parse($entities['datetime']['value']);
-
-//            $botMan->reply('You wanted to create a plan for ' . (string) $planTime);
-        })->middleware(Wit::create(env('WIT_AI_TOKEN')));
+//        $botMan->hears('create_plan', function (BotMan $botMan) {
+//            $entities = $botMan->getMessage()->getExtras('entities');
+//            $botMan->reply('Received entities: ' . json_encode($entities));
+////            $planTime = Carbon::parse($entities['datetime']['value']);
+//
+////            $botMan->reply('You wanted to create a plan for ' . (string) $planTime);
+//        })->middleware(Wit::create(env('WIT_AI_TOKEN')));
 
         $botMan->listen();
 
