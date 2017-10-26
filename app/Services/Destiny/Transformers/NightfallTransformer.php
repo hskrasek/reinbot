cb215ac09dc2c2f351f550e566b53f1f321417b2
Hunter Skrasek <hunterskrasek@me.com>
2017-10-25 19:47:46 -0500

    Add classes to transform milestones to Slack attachments
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

---
+++ app/Services/Destiny/Transformers/NightfallTransformer.php
@@ -0,0 +1,33 @@
+<?php namespace App\Services\Destiny\Transformers;
+
+class NightfallTransformer
+{
+    public function __invoke(array $milestone): array
+    {
+        $thumbUrl = array_get($milestone, 'availableQuests.0.displayProperties.icon', '');
+        $imageUrl = array_get($milestone, 'availableQuests.0.pgcr_image', '');
+
+        return [
+            'title'     => array_get($milestone, 'availableQuests.0.activity.displayProperties.name', ''),
+            'text'      => array_get($milestone, 'availableQuests.0.activity.displayProperties.description', ''),
+            'thumb_url' => empty($thumbUrl) ? $thumbUrl : 'https://www.bungie.net' . $thumbUrl,
+            'image_url' => empty($imageUrl) ? $imageUrl : 'https://www.bungie.net' . $imageUrl,
+            'fields'    => $this->buildChallengesArray($milestone),
+        ];
+    }
+
+    private function buildChallengesArray($milestone)
+    {
+        return collect(array_get($milestone, 'availableQuests.0.challenges'))->filter(function ($challenge) use (
+            $milestone
+        ) {
+            return array_get($challenge, 'activityHash') === array_get($milestone, 'availableQuests.0.activity.activityHash');
+        })->map(function (array $challenge) {
+            return [
+                'title' => array_get($challenge, 'displayProperties.name', ''),
+                'value' => array_get($challenge, 'displayProperties.description', ''),
+                'short' => true,
+            ];
+        })->toArray();
+    }
+}
