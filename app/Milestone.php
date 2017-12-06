e6a86ff33a95a0cac75215ea0fef25f25ffd2c7a
Hunter Skrasek <hunterskrasek@me.com>
2017-12-05 18:47:42 -0600

    Add helper method to lookup milestones, add doc blocks
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Milestone.php
+++ app/Milestone.php
@@ -4,6 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Milestone
+ *
+ * @property int $id
+ * @property array $json
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Milestone byBungieId($id)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Milestone whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Milestone whereJson($value)
+ * @mixin \Eloquent
+ */
 class Milestone extends Model
 {
     protected $table = 'DestinyMilestoneDefinition';
@@ -11,6 +21,15 @@ class Milestone extends Model
     protected $connection = 'destiny_manifest';
 
     protected $casts = [
-        'json' => 'json'
+        'json' => 'json',
     ];
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $id
+     */
+    public function scopeByBungieId($query, $id)
+    {
+        return $query->where('id', $id)->orWhere('id', $id - 4294967296);
+    }
 }
