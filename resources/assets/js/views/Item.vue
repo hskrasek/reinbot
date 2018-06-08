e52320b3667c01332ed10c43daa33dce118d7989
Hunter Skrasek <hunterskrasek@me.com>
2018-06-07 22:17:28 -0500

    Update the Item vue template to show sockets
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- resources/assets/js/views/Item.vue
+++ resources/assets/js/views/Item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-wrap self-center">
-        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-md my-3 bg-grey-dark">
+        <div class="rounded rounded-t-lg overflow-hidden shadow w-3/5 mx-3 my-3 bg-grey-dark">
             <img v-cloak :src=itemScreenshot :alt=item.name class="w-full" v-if="item.screenshot">
             <div class="flex justify-center -mt-8">
                 <img v-cloak :src=itemIcon :alt=item.name class="rounded-full border-solid border-white border-2 -mt-3 w-32 h-32">
@@ -12,7 +12,12 @@
                 </p>
             </div>
         </div>
-        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-md my-3 bg-grey-dark" v-if="item.lore">
+        <div class="rounded rounded-t-lg overflow-hidden shadow w-1/5 mx-3 my-3 bg-grey-dark">
+            <div class="text-center px-3 pb-4 pt-2">
+                <img v-for="socket in item.sockets" :src=socketIcon(socket) :alt=socket.name class="rounded-full border-solid border-white border-2 w-16 h-16 mr-2">
+            </div>
+        </div>
+        <div class="rounded rounded-t-lg overflow-hidden shadow max-w mx-3 my-3 bg-grey-dark" v-if="item.lore">
             <div class="text-center px-3 pb-4 pt-2">
                 <h3 class="text-white text-md bold font-sans">Lore</h3>
                 <p class="mt-2 font-sans font-medium text-white" v-html="formattedLore">
@@ -53,7 +58,11 @@
                         this.loading = false;
                         this.error = error.response.data.message || error.message;
                     });
-            }
+            },
+
+            socketIcon(socket) {
+                return 'https://www.bungie.net' + socket.icon;
+            },
         },
 
         computed: {
@@ -61,7 +70,7 @@
                 return 'https://www.bungie.net' + this.item.icon;
             },
 
-            activityScreenshot() {
+            itemScreenshot() {
                 return 'https://www.bungie.net' + this.item.screenshot;
             },
 
