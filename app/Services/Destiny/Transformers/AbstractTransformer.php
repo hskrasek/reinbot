304a5b980a016282c2680d60b41c2572d4e67074
Hunter Skrasek <hunterskrasek@me.com>
2018-09-02 11:04:32 -0500

    Add logic for posting the timeframe of a milestone
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Services/Destiny/Transformers/AbstractTransformer.php
+++ app/Services/Destiny/Transformers/AbstractTransformer.php
@@ -1,5 +1,6 @@
 <?php namespace App\Services\Destiny\Transformers;
 
+use App\Milestone;
 use Image;
 
 abstract class AbstractTransformer
@@ -19,5 +20,14 @@ abstract class AbstractTransformer
         }, 10080, true);
 
         return asset('storage/' . basename($thumbUrl));
+    }
+
+    protected function getMilestoneTimeframe(Milestone $milestone): array
+    {
+        return [
+            'title' => 'Time Frame',
+            'value' => $milestone->startDate->format('D, M d \@ H:i') . ' to '. $milestone->endDate->format('D, M d \@ H:i'),
+            'short' => true,
+        ];
     }
 }
