4d9aff81d34339811d453ec8be3b15dcfe165c2d
Hunter Skrasek <hunterskrasek@me.com>
2018-09-02 11:07:06 -0500

    Fix the ClanXPTransformer
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Services/Destiny/Transformers/ClanXPTransformer.php
+++ app/Services/Destiny/Transformers/ClanXPTransformer.php
@@ -3,19 +3,16 @@
 namespace App\Services\Destiny\Transformers;
 
 use App\Milestone;
-use App\Quest;
 
 class ClanXPTransformer extends AbstractTransformer
 {
     public function __invoke(Milestone $milestone): array
     {
-        /** @var Quest $quest */
-        $quest    = $milestone->quests->first();
-        $thumbUrl = data_get($quest, 'json.displayProperties.icon', '');
+        $thumbUrl = data_get($milestone, 'json.displayProperties.icon', '');
 
         return [
-            'title'     => data_get($quest, 'json.displayProperties.name', ''),
-            'text'      => array_get($milestone, 'json.displayProperties.description', ''),
+            'title'     => data_get($milestone, 'json.displayProperties.name', ''),
+            'text'      => data_get($milestone, 'json.displayProperties.description', ''),
             'thumb_url' => empty($thumbUrl) ? $thumbUrl : $this->getInvertedIcon($thumbUrl),
             'color'     => '#121C4D',
         ];
