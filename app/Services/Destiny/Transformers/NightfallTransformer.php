16238a95cb81ba4609a767b940a65a97bb7e6fc4
Hunter Skrasek <hunterskrasek@me.com>
2018-09-04 23:16:28 -0500

    Update Nightfall transformer
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- app/Services/Destiny/Transformers/NightfallTransformer.php
+++ app/Services/Destiny/Transformers/NightfallTransformer.php
@@ -9,29 +9,24 @@ class NightfallTransformer extends AbstractTransformer
 {
     public function __invoke(Milestone $milestone): array
     {
-        /** @var Quest $quest */
-        $quest = $milestone->quests->first();
-        /** @var Activity $activity */
-        $activity = $quest->activity;
-
-        $thumbUrl = data_get($quest, 'json.displayProperties.icon', '');
-        $imageUrl = data_get($activity, 'json.pgcrImage', '');
+        $thumbUrl = data_get($milestone, 'json.displayProperties.icon', '');
+        $imageUrl = data_get($milestone, 'json.image', '');
 
         return [
             'title'     => data_get(
-                $activity,
+                $milestone,
                 'json.displayProperties.name',
                 data_get($milestone, 'json.displayProperties.name')
             ),
-            'title_link' => config('app.url') . '/destiny2/activities/' . data_get($activity, 'json.hash'),
+            // 'title_link' => config('app.url') . '/destiny2/activities/' . data_get($activity, 'json.hash'),
             'text'      => data_get(
-                $activity,
+                $milestone,
                 'json.displayProperties.description',
                 data_get($milestone, 'json.displayProperties.description')
             ),
             'thumb_url' => empty($thumbUrl) ? $thumbUrl : $this->getInvertedIcon($thumbUrl),
             'image_url' => empty($imageUrl) ? $imageUrl : 'https://www.bungie.net' . $imageUrl,
-            'fields'    => $this->buildModifierArray($activity),
+            // 'fields'    => $this->buildModifierArray($activity),
             'color'     => '#526283',
         ];
     }
